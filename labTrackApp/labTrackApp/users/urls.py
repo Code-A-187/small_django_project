1e834b949ad5f5e3662124e972cd276b00d71100
Anton Simeonov <aplamenov@gmail.com>
2025-11-28 08:01:59 +0200

    Saveing user in DB, refactoring, URLS

--- labTrackApp/labTrackApp/users/urls.py
+++ labTrackApp/labTrackApp/users/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home-page'),
     path('register/', views.register, name='register-page'),
     path('login/', views.login, name='login-page'),
 ]
