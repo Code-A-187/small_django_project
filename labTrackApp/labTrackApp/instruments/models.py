ad08b8f4496d74a2d5e1f8efd022c728cdb1959a
Anton Simeonov <aplamenov@gmail.com>
2025-12-03 20:20:43 +0200

    instruments and MaintenanceRecord models

--- labTrackApp/labTrackApp/instruments/models.py
+++ labTrackApp/labTrackApp/instruments/models.py
@@ -7,19 +7,21 @@ STATUS = [
 ]
 class Instrument(models.Model):
     brand = models.CharField(
-        max_length=30
+        max_length=30,
+        blank=True,
     )
 
     model = models.CharField(
-        max_length=30
+        max_length=30,
     )
 
     serial_number = models.CharField(
-        max_length=50
+        max_length=50,
+        unique=True,
     )
 
     description = models.TextField(
-        max_length = 150
+        max_length = 150,
     )
 
     status = models.CharField(
@@ -28,8 +30,47 @@ class Instrument(models.Model):
         default='operational',
     )
 
-    
+    date_acquired = models.DateField(
+        auto_now_add=True
+        )
+
 
     def __str__(self):
         return self.model
+
+class MaintenanceRecord(models.Model):
+
+    instrument = models.ForeignKey(
+        Instrument,
+        on_delete=models.CASCADE,
+        related_name='maintenance_history'
+    )
+
+    maintenance_date = models.DateField()
+
+    description = models.TextField()
+
+    cost = models.DecimalField(
+        max_digits=10, 
+        decimal_places=2, 
+        null=True, 
+        blank=True,
+        )
+    
+    performed_by = models.CharField(
+        max_length=100, 
+        verbose_name="Performed By (Internal/Vendor)",
+        )
+    
+    # Optional field to track if it was routine calibration or repair
+    record_type = models.CharField(
+        max_length=10, 
+        choices=[('CAL', 'Validation'), 
+                 ('RPR', 'Repair'), 
+                 ('SVC', 'Service'),
+                 ]
+    )
+
+    def __str__(self):
+        return f"Maintenance for {self.instrument.model} with {self.instrument.serial_number} on {self.maintenance_date}"
 
